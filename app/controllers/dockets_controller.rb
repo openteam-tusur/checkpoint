c4ab19b128f91a5348b3b4334a2c3c7527e28804
Manin Iliy <bkmzvf@gmail.com>
2013-10-10 16:17:37 +0700

    docket form

--- app/controllers/dockets_controller.rb
+++ app/controllers/dockets_controller.rb
@@ -1,5 +1,6 @@
 class DocketsController < ApplicationController
   inherit_resources
   load_and_authorize_resource
-  actions :all, :except => [:index, :destroy]
+  belongs_to :subdivision
+  actions :all, :except => [:index, :destroy, :new, :create]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Checkpoint::Application.routes.draw do
   resources :permissions,   :except => [:show, :edit, :update]
 
   resources :subdivisions,  :only   => [:index, :show] do
-    resources :dockets, :except => [:index, :destroy]
+    resources :dockets, :except => [:index, :destroy, :new, :create]
   end
 
   resources :users,         :only   => [:index]
