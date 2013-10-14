2e5b8e7b23f492f907fb492af2d925669eae2db0
Ivan Puchkov <rake.eclipse@gmail.com>
2013-10-14 15:16:22 +0700

    swap :arrow_up_down:

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-require 'whenever/capistrano'
 require 'openteam/capistrano/recipes'
+require 'whenever/capistrano'
 
 set :shared_children, fetch(:shared_children) + %w[public/files]
 set :default_stage, :tusur
