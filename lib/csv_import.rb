2d6ee00a96f5f97a0c30ec4ab9c5e543151d5fe2
Ivan Puchkov <rake.eclipse@gmail.com>
2013-10-14 10:06:02 +0700

    fix import from csv

--- lib/csv_import.rb
+++ lib/csv_import.rb
@@ -6,13 +6,13 @@ class CsvImport < Struct.new(:file, :imported_object)
       surname, name, patronymic = item['ФИО студента'].split(' ')
       student = imported_object.students.find_by_name_and_surname_and_patronymic(name, surname, patronymic)
       grade = student.grades.find_or_create_by_docket_id(imported_object.id)
-      grade.update_attributes(:mark => item['Оценка'], :active => true)
+      grade.update_attributes(:mark => item['Оценка'].to_i, :active => true)
     end
   end
 
   def prepare_hash
-    csv_data = CSV.read(file)
-    headers = csv_data.shift.map {|i| i.strip.to_s }
+    csv_data = CSV.read(file, encoding: 'cp1251', :col_sep => ';')
+    headers = csv_data.shift(7).last.map {|i| i.strip.to_s }
     string_data = csv_data.map {|row| row.map {|cell| cell.to_s.strip } }
     string_data.map {|row| Hash[*headers.zip(row).flatten] }
   end
