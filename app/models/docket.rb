56042189fdf66d00924867a2d0826a64b762be43
Ivan Puchkov <rake.eclipse@gmail.com>
2013-10-23 11:09:58 +0700

    move abbr to docket model

--- app/models/docket.rb
+++ app/models/docket.rb
@@ -23,6 +23,33 @@ class Docket < ActiveRecord::Base
     filled_marks?
   end
 
+  def abbr
+    ignored = %w[при из в и у над без до к на по о от при с]
+    vocals = %w[а е ё и о у ы э ю я]
+    words = (self.discipline.gsub(/\(.+\)|\/.+\Z|\*|\\.+\Z|:.+\Z|\.|\d+|-/, ' ').gsub(/["']/, '').squish.split(' ') - ignored)
+
+    if words.one?
+      word = words.first
+      if word.size > 6
+        short_word = []
+        word.split('').each_with_index do |w, index|
+          if index <= 5
+            short_word << w
+          elsif index > 5 && vocals.include?(short_word.last)
+            short_word << w
+          else
+            return short_word.join
+          end
+        end
+        short_word.join
+      else
+        word
+      end
+    else
+      words.map{ |w| w.size > 3 ? w.first.mb_chars.upcase : w }.join
+    end
+  end
+
 private
   def clear_grades
     self.grades.inactive.update_all(:mark => nil)
--- lib/csv_export.rb
+++ lib/csv_export.rb
@@ -23,7 +23,7 @@ class CsvExport < Struct.new(:exported_object)
   end
 
   def name
-    Russian.translit([abbr, exported_object.lecturer, exported_object.group].join('_').gsub(/\s+/, '_')) + '.csv'
+    Russian.translit([exported_object.abbr, exported_object.lecturer, exported_object.group].join('_').gsub(/\s+/, '_')) + '.csv'
   end
 
   private
@@ -37,32 +37,5 @@ class CsvExport < Struct.new(:exported_object)
       end
     end
     header << 'Оценка'
-  end
-
-  def abbr
-    ignored = %w[при из в и у над без до к на по о от при с]
-    vocals = %w[а е ё и о у ы э ю я]
-    words = (exported_object.discipline.gsub(/\(.+\)|\/.+\Z|\*|\\.+\Z|:.+\Z|\.|\d+|-/, ' ').gsub(/["']/, '').squish.split(' ') - ignored)
-
-    if words.one?
-      word = words.first
-      if word.size > 6
-        short_word = []
-        word.split('').each_with_index do |w, index|
-          if index <= 5
-            short_word << w
-          elsif index > 5 && vocals.include?(short_word.last)
-            short_word << w
-          else
-            return short_word.join
-          end
-        end
-        short_word.join
-      else
-        word
-      end
-    else
-      words.map{ |w| w.size > 3 ? w.first.mb_chars.upcase : w }.join
-    end
   end
 end
