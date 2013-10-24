dc1c815d43a5c457481289d44a27a8502e740e44
Ivan Puchkov <rake.eclipse@gmail.com>
2013-10-24 17:05:36 +0700

    add grades symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 require 'openteam/capistrano/recipes'
 require 'whenever/capistrano'
 
-set :shared_children, fetch(:shared_children) + %w[public/files]
+set :shared_children, fetch(:shared_children) + %w[public/files public/grades]
 set :default_stage, :tusur
