b8ab773a297adb1c572e356c17bc93b81dd29f68
Ivan Puchkov <rake.eclipse@gmail.com>
2013-11-12 11:33:21 +0700

    replace nil lecturer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131111032748) do
+ActiveRecord::Schema.define(:version => 20131112034406) do
 
   create_table "attendances", :force => true do |t|
     t.string   "kind"
