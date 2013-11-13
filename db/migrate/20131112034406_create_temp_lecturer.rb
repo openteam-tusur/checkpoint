1b43905e6de7af6e13dc15721dc6d9525c86265f
Ivan Puchkov <rake.eclipse@gmail.com>
2013-11-13 12:08:53 +0700

    chenge temp lecturer name to surname

--- db/migrate/20131112034406_create_temp_lecturer.rb
+++ db/migrate/20131112034406_create_temp_lecturer.rb
@@ -1,6 +1,6 @@
 class CreateTempLecturer < ActiveRecord::Migration
   def change
-    lecturer = Lecturer.create(:name => 'Преподаватель не указан')
+    lecturer = Lecturer.create(:surname => 'Преподаватель не указан')
     Docket.where(:lecturer_id => nil).map {|d| d.update_column(:lecturer_id, lecturer.id)}
   end
 end
