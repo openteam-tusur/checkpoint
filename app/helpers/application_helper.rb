04a2246ad89e4f34321e2219bd4c3e530530265a
Ivan Puchkov <rake.eclipse@gmail.com>
2013-11-18 12:00:24 +0700

    fix dockets download link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def summary_dockets_link(period, subdivision)
+    if period.id == 1
+      path = "/grades/#{subdivision.folder_name}"
+    else
+      path = "/grades/#{period.year}/#{period.season_type}/#{period.kind}/#{subdivision.folder_name}"
+    end
+    if File.directory?("public#{path}")
+      link_to "Скачать ведомости", "#{path}/#{subdivision.abbr_translit}.zip"
+    end
+  end
 end
