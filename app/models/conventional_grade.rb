600af5d253eb8e58d39135c62fc9426e8f9a7197
Ivan Puchkov <rake.eclipse@gmail.com>
2013-11-19 12:50:21 +0700

    revert mark order

--- app/models/conventional_grade.rb
+++ app/models/conventional_grade.rb
@@ -1,3 +1,3 @@
 class ConventionalGrade < Grade
-  enumerize :mark, :in => [0, 2, 3, 4, 5]
+  enumerize :mark, :in => [5, 4, 3, 2, 0]
 end
--- app/models/qualification_grade.rb
+++ app/models/qualification_grade.rb
@@ -1,3 +1,3 @@
 class QualificationGrade < Grade
-  enumerize :mark, :in => [0, 2, 5]
+  enumerize :mark, :in => [5, 2, 0]
 end
