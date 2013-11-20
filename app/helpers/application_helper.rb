6daa28b2639adfcc73ad9c0a77b32c5eff07be2e
Ivan Puchkov <rake.eclipse@gmail.com>
2013-11-20 10:24:51 +0700

    archive download link for all docket formats

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-  def summary_dockets_link(period, subdivision)
+  def summary_dockets_link(period, subdivision, file_format)
     if period.id == 1
       path = "/grades/#{subdivision.folder_name}"
     else
       path = "/grades/#{period.year}/#{period.season_type}/#{period.kind}/#{subdivision.folder_name}"
     end
     if File.directory?("public#{path}")
-      link_to "Скачать ведомости", "#{path}/#{subdivision.abbr_translit}.zip"
+      link_to "Скачать #{I18n.t("docket.file_format.#{file_format}")}", "#{path}/#{subdivision.abbr_translit}_#{file_format}.zip"
     end
   end
 end
