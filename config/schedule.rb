7c8e215f6adbd4af672e072e88c0f2e8ff333f7a
Ivan Puchkov <rake.eclipse@gmail.com>
2013-11-21 08:50:12 +0700

    add tasks logging

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,7 @@
- set :output, 'log/import.log'
-
  every 1.day, :at => '5:00 am' do
-   rake :import_attendances
+   rake :import_attendances, :output => { :error => 'log/error-import_atendances.log', :standard => 'log/import_attendances.log'}
  end
 
  every 1.day, :at => '2:00 am' do
-   rake :export_grades
+   rake :export_grades, :output => { :error => 'log/error-export_grades.log', :standard => 'log/export_grades.log'}
  end
