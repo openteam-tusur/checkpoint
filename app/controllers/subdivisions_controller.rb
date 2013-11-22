ebf9ad195c31daf0ef842da06bd51008be15b658
Dmitriy Baraksanov <bdn@openteam.ru>
2013-11-22 15:14:34 +0700

    check ability on list subdivisions

--- app/controllers/subdivisions_controller.rb
+++ app/controllers/subdivisions_controller.rb
@@ -4,6 +4,12 @@ class SubdivisionsController < ApplicationController
   actions :index, :show
   has_scope :by_title, :default => 1
 
+  def index
+    index! {
+      authorize! :manage, Subdivision
+    }
+  end
+
   def show
     show!{
       @periods = Period.all.select {|p| can?(:read, p) && @subdivision.dockets.by_period(p.id).any?}
