5084a01c99dfb342c0e17a01159f637dcb03afe2
Ivan Puchkov <rake.eclipse@gmail.com>
2013-12-16 10:19:55 +0700

    add generate json task to crontab schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,9 +5,15 @@
  every 1.day, :at => '03:00 am' do
    rake 'export:consolidated', :output => { :error => 'log/error-export_consolidated.log', :standard => 'log/export_consolidated.log'}
  end
+
  every 1.day, :at => '04:00 am' do
    rake 'export:csv', :output => { :error => 'log/error-export_csv.log', :standard => 'log/export_csv.log'}
  end
+
  every 1.day, :at => '05:00 am' do
    rake 'export:pdf', :output => { :error => 'log/error-export_pdf.log', :standard => 'log/export_pdf.log'}
+ end
+
+ every 1.day, :at => '06:00 am' do
+   rake :export_json, :output => { :error => 'log/error-export_json.log', :standart => 'log/export_json.log' }
  end
