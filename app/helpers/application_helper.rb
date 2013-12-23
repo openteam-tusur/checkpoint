def3bf8aa1433e1149785e4a9dd6ad41c308c0a6
Ivan Puchkov <rake.eclipse@gmail.com>
2013-12-23 16:17:01 +0700

    fix download links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,16 @@ module ApplicationHelper
     end
     file_name = "#{subdivision.abbr_translit}_#{file_format}"
     if File.exist?("public#{path}/#{file_name}.zip")
-      link_to "Скачать #{I18n.t("docket.file_format.#{file_format}")}", "#{path}/#{file_name}.zip"
+      content_tag(:li, link_to("Скачать #{I18n.t("docket.file_format.#{file_format}")}", "#{path}/#{file_name}.zip"))
+    end
+  end
+
+  def summary_dockets_links
+    [].tap do |arr|
+      arr << summary_dockets_link(@period, @subdivision, 'pdf') unless @period.not_session?
+      arr << summary_dockets_link(@period, @subdivision, 'consolidated_pdf')
+      arr << summary_dockets_link(@period, @subdivision, 'consolidated_xls')
+      arr << summary_dockets_link(@period, @subdivision, 'csv') if (@period.not_session? && @period.editable?)
     end
   end
 end
