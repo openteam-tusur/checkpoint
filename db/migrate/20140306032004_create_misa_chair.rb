5bc27fa1c48bb349e388227533171f8631338b8f
Ivan Puchkov <rake.eclipse@gmail.com>
2014-03-13 10:27:03 +0700

    fix create misa chair migration

--- db/migrate/20140306032004_create_misa_chair.rb
+++ db/migrate/20140306032004_create_misa_chair.rb
@@ -1,5 +1,5 @@
 class CreateMisaChair < ActiveRecord::Migration
   def change
-    Subdivision::Chair.create(:title => 'Кафедра моделирования и системного анализа', :abbr => 'МиСА')
+    Subdivision::Chair.find_or_create_by_title_and_abbr(:title => 'Кафедра моделирования и системного анализа', :abbr => 'МиСА')
   end
 end
