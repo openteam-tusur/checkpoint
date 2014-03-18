885f65e733b00d124028e3e30ceea92ef6972354
Ivan Puchkov <rake.eclipse@gmail.com>
2014-03-18 12:06:25 +0700

    remove commented strings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,17 +6,17 @@ module ApplicationHelper
       path = "/grades/#{period.year}/#{period.season_type}/#{period.kind}_#{period.id}/#{subdivision.folder_name}"
     end
     file_name = "#{subdivision.abbr_translit}_#{file_format}"
-    #if File.exist?("public#{path}/#{file_name}.zip")
+    if File.exist?("public#{path}/#{file_name}.zip")
       content_tag(:li, link_to("Скачать #{I18n.t("docket.file_format.#{file_format}")}", "#{path}/#{file_name}.zip"))
-    #end
+    end
   end
 
   def summary_dockets_links(period)
     [].tap do |arr|
-      arr << summary_dockets_link(period, @subdivision, 'pdf') #unless @period.not_session?
+      arr << summary_dockets_link(period, @subdivision, 'pdf') unless @period.not_session?
       arr << summary_dockets_link(period, @subdivision, 'consolidated_pdf')
       arr << summary_dockets_link(period, @subdivision, 'consolidated_xls')
-      arr << summary_dockets_link(period, @subdivision, 'csv') #if (@period.not_session? && @period.editable?)
+      arr << summary_dockets_link(period, @subdivision, 'csv') if (@period.not_session? && @period.editable?)
     end
   end
 end
