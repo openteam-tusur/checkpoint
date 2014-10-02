d469b8e5b4f6874483ac87b56c7d637e49399a6b
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-10-02 16:43:07 +0700

    Integration with AuthServer

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -4,8 +4,4 @@ class PermissionsController < ApplicationController
   has_scope :by_user, :default => 1
   has_scope :by_role, :only => :index, :allow_blank => true
   actions :all, :except => [:show, :edit, :update]
-
-  #def collection
-    #@permissions = Permission.where(:id => [470])
-  #end
 end
