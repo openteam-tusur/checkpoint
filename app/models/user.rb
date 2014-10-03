cb5db4f3cd9db55f439d167f93686edc86c034e6
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-10-03 09:44:45 +0700

    Some methods for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User
   include AuthClient::User
 
+  def self.with_permissions(role)
+    Permission.where.not(:user_id => nil).where(:role => role).uniq
+  end
+
   def app_name
     'checkpoint'
+  end
+
+  def subdivisions
+    Subdivision.joins(:permissions).where('permissions.user_id' => id).uniq
+  end
+
+  def lecturers
+    Lecturer.joins(:permissions).where('permissions.user_id' => id).uniq
   end
 end
