0e104dd3dcf2df7e2ea9bd45b1a77d28d9f4aad0
Ivan Puchkov <rake.eclipse@gmail.com>
2014-10-09 10:19:24 +0700

    fix permission managing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
+require 'open-uri'
+
 class UsersController < ApplicationController
   inherit_resources
   load_and_authorize_resource
-  has_scope :by_surname, :default => 1
+  has_scope :by_surname, :default => 1, :only => :index
   actions :index
   layout false
   respond_to :json
 
   def collection
     super.where("email ILIKE ?", "%#{params[:term]}%" )
+  end
+
+  def search
+    url = Settings['auth_server.users_url'] + "?term=#{params[:term]}"
+
+    result = open(URI.encode(url)).read
+
+    render :json => result
   end
 end
