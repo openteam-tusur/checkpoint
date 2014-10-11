ab8482448e4c3cb19ecf673f7955bca4f1c28322
Ivan Puchkov <rake.eclipse@gmail.com>
2014-10-11 10:50:08 +0700

    another fix for users serach url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   def search
-    url = Settings['profile.users_url'] + "?term=#{params[:term]}"
+    url = "#{Settings['profile.url']}/users/search?term=#{params[:term]}"
 
     result = open(URI.encode(url)).read
 
