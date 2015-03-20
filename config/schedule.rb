88522c65fdbf3cbdef0ee814593fcd18b2fa7ddb
Ivan Puchkov <rake.eclipse@gmail.com>
2015-03-20 11:13:44 +0600

    change crontab schedule time

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,23 +2,23 @@
    rake :import_attendances, :output => { :error => 'log/error-import_attendances.log', :standard => 'log/import_attendances.log'}
  end
 
- every 1.day, :at => '03:00 am' do
+ every 1.day, :at => '05:00 am' do
    rake 'export:consolidated_pdf', :output => { :error => 'log/error-export_consolidated_pdf.log', :standard => 'log/export_consolidated_pdf.log'}
  end
 
- every 1.day, :at => '04:00 am' do
+ every 1.day, :at => '06:00 am' do
    rake 'export:consolidated_xls', :output => { :error => 'log/error-export_consolidated_xls.log', :standard => 'log/export_consolidated_xls.log'}
  end
 
- every 1.day, :at => '05:00 am' do
+ every 1.day, :at => '07:00 am' do
    rake 'export:csv', :output => { :error => 'log/error-export_csv.log', :standard => 'log/export_csv.log'}
  end
 
- every 1.day, :at => '06:00 am' do
+ every 1.day, :at => '08:00 am' do
    rake 'export:pdf', :output => { :error => 'log/error-export_pdf.log', :standard => 'log/export_pdf.log'}
  end
 
- every 1.day, :at => '07:00 am' do
+ every 1.day, :at => '02:00 am' do
    rake :export_json, :output => { :error => 'log/error-export_json.log', :standart => 'log/export_json.log' }
  end
 
