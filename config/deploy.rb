c826f5b0dbb811525d2de1287bbe032a23018ad2
Evgeny Lapin <lev@openteam.ru>
2016-12-02 11:17:59 +0700

    changed config for new capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,12 @@
 require 'openteam/capistrano/deploy'
 
-set :linked_dirs, fetch(:linked_dirs) + %w{ public/files public/grades }
+set :bundle_binstubs, -> { shared_path.join('bin') }
+
+set :db_remote_clean, true
+
+append :linked_dirs, 'public/files'
+append :linked_dirs, 'public/grades'
+
+set :slackistrano,
+  channel: (Settings['slack.channel'] rescue ''),
+  webhook: (Settings['slack.webhook'] rescue '')
