96d2d9a54a8fa3b2ddc4596142350236a77b776b
Evgeny Lapin <lev@openteam.ru>
2016-12-02 11:08:56 +0700

    removed two initializers

--- config/initializers/_read_settings.rb
+++
@@ -1,6 +0,0 @@
-Settings.read(Rails.root.join('config', 'settings.yml'))
-Settings.define 'app.secret',               :required => true
-Settings.define 'errors.key',               :required => Rails.env.production?
-Settings.define 'errors.host',              :required => Rails.env.production?
-
-Settings.resolve!
